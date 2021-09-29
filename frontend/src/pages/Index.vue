5d1a4b435d4c3cdf5a2fbb410524cb06372b3839
Alex Zdrafcu <alexzdrafcu@gmail.com>
2021-09-29 15:08:41 +0300

    index

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -1,6 +1,13 @@
 <template>
-  <div class="row items-center justify-evenly">
-    <q-btn color="primary" label="intrați" to="/categories" />
+  <div class="index-page row items-center justify-evenly">
+      <q-btn
+        color="white text-black text-bold"
+        flat
+        rounded
+        class=""
+        label="Get drunk"
+        to="/categories"
+      />
   </div>
 </template>
 
@@ -14,3 +21,25 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap");
+
+.index-page {
+  font-family: "Raleway", sans-serif;
+  background: #f3bfdf;
+  height: 100vh;
+  
+  background-image: url("../assets/indexpage.png");
+  background-position: center; /* Center the image */
+  background-repeat: no-repeat; /* Do not repeat the image */
+  background-size: 350px;
+}
+
+.q-btn {
+  background: #ffffff;
+  box-shadow: 4px 4px 0px #0926d3;
+  top: 255px;
+  left: 80px;
+}
+</style>
